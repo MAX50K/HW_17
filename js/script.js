4fc6e6df2aca4f3460efd5954002424e545b53b7
MAX50K <146106579+MAX50K@users.noreply.github.com>
2025-05-30 12:43:14 +0200

    wsw

--- js/script.js
+++ js/script.js
@@ -1,1 +1,5 @@
-    
+const button = document.getElementById('button');
+const input = document.getElementById('input');
+button.addEventListener('click', function () {
+  button.textContent = input.value;
+});
