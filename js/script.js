871fdceeac6a1f626e2dfd91a6677afd4977d1d6
MAX50K <146106579+MAX50K@users.noreply.github.com>
2025-06-01 14:11:04 +0200

    sw

--- js/script.js
+++ js/script.js
@@ -1,6 +1,18 @@
-const textInput = document.getElementById('textInput');
-const changeButton = document.getElementById('changeButton');
-changeButton.addEventListener('click', () => {
+const textInput = document.getElementById("textInput");
+const changeButton = document.getElementById("changeButton");
+changeButton.addEventListener("click", () => {
   changeButton.textContent = textInput.value;
 }); 
 // 2
+const image = document.getElementById("image");
+image.src = "../nice_cat.png";
+// 3
+const link = document.getElementById("link");
+const img = document.getElementById("linkImage");
+
+link.href = "https://google.com";
+img.alt = "Це логотип Google";
+// 4
+const list = document.getElementById("list");
+const firstItem = list.querySelector("li");
+firstItem.textContent = "Lirli Larila";
